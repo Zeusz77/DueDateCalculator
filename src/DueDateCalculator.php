956754d49f32a3dcd1b3e151279e81e39d860b20
Zeusz77 <aron.franta@gmail.com>
2022-01-08 23:05:31 +0100

    Cleanup

--- src/DueDateCalculator.php
+++ src/DueDateCalculator.php
@@ -2,16 +2,6 @@
 
 final class DueDateCalculator
 {
-    private $date_started;
-    private $turnaround_time;
-
-    private function _construct(string $date, int $turnaround)
-    {
-        $this->$date_started = $date;
-
-        $this->$turnaround_time = $turnaround;
-    }
-
     public function calculate_due_date(string $date, int $turnaround) : string
     {
         $report_hour = (int)date("H", strtotime($date));
@@ -26,7 +16,7 @@ final class DueDateCalculator
         {
             $current_hour = (int)date("H", $final_date);
             $current_day = date("D", $final_date);
-            
+
             if(
                 ($current_hour >= 9 && $current_hour <= 16) && ($current_day != 'Sat' && $current_day != 'Sun')
             ) $turnaround--;
@@ -34,6 +24,6 @@ final class DueDateCalculator
             $final_date = strtotime("+1 hour", $final_date);
         }
 
-        return date("Y-m-d H:i D", $final_date);
+        return date("Y-m-d H:i", $final_date);
     }
 }
--- tests/DueDateCalculatorTest.php
+++ tests/DueDateCalculatorTest.php
@@ -13,24 +13,24 @@ final class DueDateCalculatorTest extends TestCase
     public function test_basic_assertion(): void
     {
         $this->assertEquals(
-            "2016-10-17 15:48 Mon",
-            DueDateCalculator::calculate_due_date("2016-10-17 14:48:21", 1)
+            "2016-10-17 15:48",
+            DueDateCalculator::calculate_due_date("2016-10-17 14:48", 1)
         );
     }
 
     public function test_whole_day_turnaraound(): void
     {
         $this->assertEquals(
-            "2016-10-19 14:48 Wed",
-            DueDateCalculator::calculate_due_date("2016-10-17 14:48:21", 16)
+            "2016-10-19 14:48",
+            DueDateCalculator::calculate_due_date("2016-10-17 14:48", 16)
         );
     }
 
     public function test_turnaround_over_not_whole_days(): void
     {
         $this->assertEquals(
-            "2016-10-18 16:48 Wed",
-            DueDateCalculator::calculate_due_date("2016-10-17 14:48:21", 10)
+            "2016-10-18 12:48",
+            DueDateCalculator::calculate_due_date("2016-10-17 14:48", 6)
         );
     }
 
