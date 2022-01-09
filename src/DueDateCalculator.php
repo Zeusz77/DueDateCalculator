5318c8870227523ca763733a73c0af68a6dcf54c
Zeusz77 <aron.franta@gmail.com>
2022-01-09 10:37:13 +0100

    readability update

--- src/DueDateCalculator.php
+++ src/DueDateCalculator.php
@@ -6,14 +6,20 @@ final class DueDateCalculator
     {
 
         // Exception handling
-        if(!strtotime($date)) throw new InvalidArgumentException('Date given was invalid format!');
+        if(!strtotime($date))
+        {
+            throw new InvalidArgumentException('Date given was invalid format!');
+        }
 
         $report_hour = (int)date("H", strtotime($date));
         $report_day = (int)date("D", strtotime($date));
         
         if(
             ($report_hour < 9 || $report_hour > 16) || ($report_day != 'Sat' && $report_day != 'Sun')
-        ) throw new InvalidArgumentException('Date given was outside service hours!');
+        )
+        {
+            throw new InvalidArgumentException('Date given was outside service hours!');
+        } 
 
         // Solution
         $final_date = strtotime($date);
@@ -26,8 +32,11 @@ final class DueDateCalculator
             // The turnaround is only decresade if it's a working hour on a non-weekend day
             if(
                 ($current_hour > 8 && $current_hour < 17) && ($current_day != 'Sat' && $current_day != 'Sun')
-            ) $turnaround--;
-            
+            )
+            {
+                $turnaround--;
+            }
+
             $final_date = strtotime("+1 hour", $final_date);
         }
 
